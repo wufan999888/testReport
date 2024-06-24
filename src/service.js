04640e6ff07053cb974917aba9c6777b286cd29a
wufan <wufan@alluu.com>
2024-06-25 01:42:18 +0800

    fix: 打包文件

--- src/service.js
+++ src/service.js
@@ -1,4 +1,4 @@
 import axios from './module/axios'; // 引入封装好的axios
-import api from "../config/api.js";
+// import api from "../config/api.js";
 // 测试
-export const test = () => (axios({ url: api.yesApi + '/?s=SVIP.Swufan999888_MyApi.AGetTestReport&return_data=0&id=1&app_key=8D97B71A9E9BD52F08B8EE0A7FB9A696', method: 'get' }))
+export const test = () => (axios({ url: 'https://hd215.api.yesapi.cn' + '/?s=SVIP.Swufan999888_MyApi.AGetTestReport&return_data=0&id=1&app_key=8D97B71A9E9BD52F08B8EE0A7FB9A696', method: 'get' }))
